3bee150bb081fe72791b92a297bc5403b81f6a51
L-T45 <loic.thibault45@gmail.com>
2020-04-29 03:58:30 +0200

    delete lines in FacturesViewModel

--- app/src/main/java/com/project/x_factories/ui/facturedevis/FacturesViewModel.java
+++ app/src/main/java/com/project/x_factories/ui/facturedevis/FacturesViewModel.java
@@ -15,34 +15,8 @@ public class FacturesViewModel extends ViewModel {
         mText.setValue("Création d'une Facture ou d'un Devis" +
         "\n Il est recommandé d'avoir l'application Google Sheets sur votre appareil avant de " +
                 "cliquer sur le lien ci-dessous.");
-
-
-        /*
-        // This code uses the Sheets Advanced Service, but for most use cases
-        // the built-in method SpreadsheetApp.getActiveSpreadsheet()
-        //     .getRange(range).getValues(values) is more appropriate.
-        ranges = [
-        // Range names ...
-            ];
-        result = Sheets.Spreadsheets.Values.batchGet(spreadsheetId, {ranges:ranges});
-        // This code uses the Sheets Advanced Service, but for most use cases
-        // the built-in method SpreadsheetApp.getActiveSpreadsheet()
-        //     .getRange(range).setValues(values) is more appropriate.
-        valeurs = [
-            [
-        // Cell values ...
-            ]
-        // Additional rows ...
-        ];
-        valueRange = Sheets.newValueRange();
-        valueRange.range = range;
-        valueRange.values = valeurs;
-
-        batchUpdateRequest = Sheets.newBatchUpdateValuesRequest();
-        batchUpdateRequest.data = valueRange;
-        batchUpdateRequest.valueInputOption = valueInputOption;
-
-        result = Sheets.Spreadsheets.Values.batchUpdate(batchUpdateRequest, spreadsheetId);*/
+        // Ajout d'un lien cliquable dans fragment_factures.xml
+        
     }
     public LiveData<String> getText() {
         return mText;
